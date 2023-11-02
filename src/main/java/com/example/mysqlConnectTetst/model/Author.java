3012421b33a1bbac49304896b5651744248f9fdd
leek94 <khwan929@naver.com>
2023-11-02 12:17:08 +0900

    feat: model/Author 생성

---
+++ src/main/java/com/example/mysqlConnectTetst/model/Author.java
@@ -0,0 +1,19 @@
+package com.example.mysqlConnectTetst.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
