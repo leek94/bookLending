7dbd011217c144e2699159511a36ef8868de6d8b
leek94 <khwan929@naver.com>
2023-11-02 11:53:34 +0900

    init 생성

---
+++ src/main/java/com/example/mysqlConnectTetst/MysqlConnectTetstApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlConnectTetst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectTetstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlConnectTetstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysqlConnectTetst/MysqlConnectTetstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlConnectTetst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnectTetstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
